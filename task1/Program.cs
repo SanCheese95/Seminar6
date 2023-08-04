4181a80e3b4b8181a9ffa6e18d1e8beab7cc9042
Sancheese95 <sasha_pavlyukovec@mail.ru>
2023-08-05 01:16:42 +0300

    done homework

---
+++ task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество элементов массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[num];
+
+void PrintArray(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива ");
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int FindCount(int[] Array)
+{
+    int i = 0;
+    int count = 0;
+    while (i < Array.Length)
+    {
+        if (Array[i] > 0)
+            count++;
+        i++;
+    }
+    return count;
+}
+
+PrintArray(num);
+Console.Write($"Чисел больше нуля: {FindCount(Array)}");
---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double GetInfo (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+double num = GetInfo("введите значение b1: ");
+double num2 = GetInfo("введите число k1: ");
+double num3 = GetInfo("введите значение b2: ");
+double num4 = GetInfo("введите число k2: ");
+
+double x = (-num3 + num)/(-num2 + num4);
+double y = num4 * x + num3;
+
+Console.WriteLine($"две прямые пересекутся в точке c координатами X: {x}, Y: {y}");
